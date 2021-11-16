d20ac43c0d5efed2f49014e07e0d6111dc69b246
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-11-15 23:51:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+
+function submitData(userName, userEmail) {
+    const submittedData = JSON.stringify( {
+        name: userName,
+        email: userEmail
+    })
+    const someConfig = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: submittedData
+    }
+    
+    fetch("http://localhost:3000/users", someConfig)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (object) {
+        document.body.append(object.id);
+    })
+    .catch(function (error) {
+        document.body.append(error)
+    })
+}
